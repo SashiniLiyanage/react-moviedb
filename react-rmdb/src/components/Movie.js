6ebbe3d61da13fc32f15b5b7df6992ffb54ec477
“sashininliyanage” <nimthara.liyanage@gmail.com>
2021-12-01 22:09:33 +0530

    add movie header

--- react-rmdb/src/components/Movie.js
+++ react-rmdb/src/components/Movie.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+// Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+import BreadCrump from './BreadCrump';
+
+// Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
+
+
+// Image
+import NoImage from '../images/no-image.jpg';
 
 const Movie = () => {
-    return (
-        <div>
-           Movie 
-        </div>
-    )
-}
+  const { movieId } = useParams();
+  
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  console.log(movie);
+
+  if(loading) return <Spinner/>
+  if(error) return <div>Something went wrong</div>
+  
+  return (
+    <>
+      <BreadCrump movieTitle={movie.original_title}/>
+    </>
+  );
+};
 
 export default Movie;
