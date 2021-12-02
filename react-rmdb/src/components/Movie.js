ea598d630ee70b2545cdbf894a3cb448638c01e9
“sashininliyanage” <nimthara.liyanage@gmail.com>
2021-12-02 16:37:33 +0530

    add actor grid

--- react-rmdb/src/components/Movie.js
+++ react-rmdb/src/components/Movie.js
@@ -8,6 +8,8 @@ import Grid from './Grid';
 import Spinner from './Spinner';
 import BreadCrump from './BreadCrump';
 import MovieInfo from './MovieInfo';
+import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 
 // Hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
@@ -30,7 +32,24 @@ const Movie = () => {
     <>
         <BreadCrump movieTitle={movie.original_title}/>
         <MovieInfo movie={movie}/>
+        <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue}/>
+        <Grid header='Actors'>
+          {movie.actors.map(actor=>(
+            <Actor
+              key={actor.credit_id}
+              name={actor.name}
+              character={actor.character}
+              imageUrl={
+                actor.profile_path ?
+                `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+              }
+
+            />
+          ))}
+    </Grid>
     </>
+    
   );
 };
 
